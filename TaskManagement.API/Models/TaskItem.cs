7722925de0789c8d487d991b039a27299bc98eef
Ibrahim <ibrahimshah064@gmail.com>
2026-05-05 23:02:25 +0500

    Created Tables and Migrated Database

---
+++ TaskManagement.API/Models/TaskItem.cs
@@ -0,0 +1,18 @@
+﻿namespace TaskManagement.API.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public string Status { get; set; } // Pending, InProgress, Completed
+        public string Priority { get; set; } // Low, Medium, High
+
+        public DateTime DueDate { get; set; }
+
+        // Foreign Key
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ TaskManagement.API/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace TaskManagement.API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+
+        public string Role { get; set; } // Admin or User
+
+        public ICollection<TaskItem> Tasks { get; set; }
+    }
+}
